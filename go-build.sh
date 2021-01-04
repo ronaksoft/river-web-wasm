15e43e52ec7d5928c212ae5834e5dd8e40c29f4c
Hamidreza KK <hamidrezakks@gmail.com>
2021-01-04 10:08:14 +0330

    build updated

--- go-build.sh
+++ go-build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-GOOS=js GOARCH=wasm go build -ldflags="-s -w" -o /Users/hamidrezakk/ronak/river/web-app/public/bin/river.wasm .
+GOOS=js GOARCH=wasm GODEBUG=gcstoptheworld=1 GOGC=20 go build -ldflags="-s -w" -o /Users/hamidrezakk/ronak/river/web-app/public/bin/river.wasm .
