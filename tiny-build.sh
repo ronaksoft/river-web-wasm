734ee3fa3589918dcabb02a9738f42249eb852af
Hamidreza KK <hamidrezakks@gmail.com>
2021-03-07 15:52:34 +0330

    build script updated

--- tiny-build.sh
+++ tiny-build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-GOOS=js GOARCH=wasm /Users/hamidrezakk/go/bin/tinygo build -no-debug -o /Users/hamidrezakk/ronak/river/web-app/public/bin/river-tiny.wasm -target wasm .
+GOOS=js GOARCH=wasm /Users/hamidrezakk/go/bin/tinygo build -no-debug -tags=math_big_pure_go -o /Users/hamidrezakk/ronak/river/web-app/public/bin/river-tiny.wasm -target wasm .
